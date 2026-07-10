4155bacfe71cb40e66ea88f056247a1e86c36ce4
Shyan Bera <shyan.bera@gmail.com>
2026-07-11 00:45:14 +0100

    Iterated the experiment for each number of paths to average the absolute errors and confirm O(N^-0.5) behaviour graphically

--- analytics.py
+++ analytics.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from pricer import monte_carlo_option_pricer, analytical_call_price
 
-def convergence_test(S_0,r,sigma,T,K):
+def convergence_test(S_0,r,sigma,T,K,num_experiments):
     true_analytical_price = analytical_call_price(S_0,r,sigma,T,K)
     path_counts = [100,1000,10000,100000,1000000]
     mc_errors=[]
@@ -11,10 +11,17 @@ def convergence_test(S_0,r,sigma,T,K):
     print(f"{'Paths (N)':<12} | {'MC Price':<10} | {'Abs Error':<10}")
     print("-" * 38)
     for N in path_counts:
-        mc_price = monte_carlo_option_pricer(S_0,r,sigma,T,K,N)
-        abs_error = abs(mc_price-true_analytical_price)
-        mc_errors.append(abs_error)
-        print(f"{N:<12} | £{mc_price:<9.2f} | £{abs_error:<10.4f}")
+        total_error = 0
+        total_mc_price = 0
+        for experiment in range(num_experiments):
+            mc_price = monte_carlo_option_pricer(S_0,r,sigma,T,K,N)
+            abs_error = abs(mc_price-true_analytical_price)
+            total_error = total_error + abs_error
+            total_mc_price = total_mc_price+mc_price
+        avg_mc_price = total_mc_price / num_experiments
+        avg_error = total_error / num_experiments
+        mc_errors.append(avg_error)
+        print(f"{N:<12} | £{avg_mc_price:<9.2f} | £{avg_error:<10.4f}")
 
     plt.figure(figsize=[10,6])
     plt.plot(path_counts,mc_errors, marker='o')
@@ -29,4 +36,4 @@ def convergence_test(S_0,r,sigma,T,K):
 
 
 if __name__ == "__main__":
-    convergence_test(100,0.05,0.2,1,100)
+    convergence_test(100,0.05,0.2,1,100,100)
